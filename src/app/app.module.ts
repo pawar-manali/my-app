2809007b52b6b0ac8ad8005397429a76a9c601f5
pawar-manali <manalip798@gmail.com>
2023-06-22 18:16:28 +0530

    side nav added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,12 @@ import { ChildComponent } from './Data-Binding-Component/parent/child/child.comp
 import { MySideNavComponent } from './my-side-nav/my-side-nav.component';
 import { RoundButtonDirective } from './round-button.directive';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -23,12 +29,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     ParentComponent,
     ChildComponent,
     MySideNavComponent,
-    RoundButtonDirective
+    RoundButtonDirective,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
